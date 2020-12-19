17dae6220b63a5dfa8d58ece14f8fdba4c6a12f2
Steve <stephenvincentibanez@gmail.com>
2020-12-19 14:58:34 -0800

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,27 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+
+    state ={
+        mood: true
+    }
+
+    toggleMood = () => {
+        this.setState((prevState)=>{
+            return{
+                mood: !prevState.mood
+            }
+        })
+      }
+
+    render() {
+        return (
+            <div onClick={this.toggleMood}>
+                {this.state.mood === true? "Happy" : "Sad"}
+            </div>
+        );
+    }
+}
+
+export default SimpleComponent;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const handleClick = () => {
+  alert("I've been clicked")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
   </div>,
   document.getElementById('root')
 );
